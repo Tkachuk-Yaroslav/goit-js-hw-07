4a3c90cbb5e462664abb327ed8afd6a62d51f3a9
Tkachuk-Yaroslav <tkachuky105@gmail.com>
2023-07-03 22:36:29 +0300

    Завдання 1 пункт 1

---
+++ js/01-gallery.js
@@ -0,0 +1,40 @@
+//імпорт з іншого файлу масиву з обєктами даних
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+//присвоююю змінній результат функції(рядок коду HTML)
+const cardsMurkup = createMurcup(galleryItems);
+
+//ловлю список в якій вставлятиму створені лішки
+const gallaryContainerElem = document.querySelector(".gallery");
+//вставляю готову розмітку в HTML
+gallaryContainerElem.insertAdjacentHTML('beforeend', cardsMurkup);
+//функція, що створює рядок з розміткою
+function createMurcup(gallarys) {
+    const murcup = gallarys.map(({preview, original, description}) => {
+        return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </li>
+        `
+    });
+    // console.log(murcup.join(''))
+    return murcup.join('');
+}
+
+//////////ДРУГА ЧАСТИНА(СПРОБУВАВ)
+gallaryContainerElem.addEventListener('click', onPhotoClick);
+
+function onPhotoClick(event) {
+    event.preventDefault();
+    if (event.target.tagName === 'IMG') {
+        console.log("Клацнули саме по картинці!")
+    }
+}
