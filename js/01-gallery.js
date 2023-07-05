c623ba47064a8056239f26c996cd42ccb3ca8337
Tkachuk-Yaroslav <tkachuky105@gmail.com>
2023-07-05 22:23:06 +0300

    Завдання 1, закриття по Escape

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -30,20 +30,37 @@ function createMurcup(gallarys) {
 }
 
 //////////ДРУГА ЧАСТИНА(СПРОБУВАВ)
+// делегую слухача подій на список
 gallaryContainerElem.addEventListener('click', onPhotoClick);
-
+//калбек для слухача
 function onPhotoClick(event) {
+    //забороняю скачвати картинки по кліку на них
     event.preventDefault();
+    //знаходжу URL до кожної картинки
     const imgURL = event.target.dataset.source;
+    //перевіряю чи клацнув саме на картинку
     if (event.target.tagName === 'IMG') {
-        console.log("Клацнули саме по картинці!")
-        
+        console.log("Клацнули саме по картинці!")        
         console.log(imgURL);
-    }
-/////////////////4 та 5 частини////////
-    const instance = basicLightbox.create(`
-    <img src="${imgURL}" width="800" height="600">
-`)
+        /////////////////4 та 5 частини////////
+        //створюю модалкуу з бібліотекою basicLightbox
+        const instance = basicLightbox.create(`
+        <img src="${imgURL}" width="800" height="600">
+        `)
+        //відкриваю модалку
+        instance.show()
 
-    instance.show()
+        /////////////Додаткове завдання////////
+        //ставлю слухача на keydown на документі
+        document.addEventListener("keydown", onKeyPress);
+        //калбек
+        function onKeyPress(evt) {
+            //якщо нажали Escape закриваємо модалку бібліотеки та видаляємо слухача
+            if (evt.key === "Escape") {
+                instance.close();
+                document.removeEventListener("keydown", onKeyPress);
+            }
+        }
+    }
+    
 }
